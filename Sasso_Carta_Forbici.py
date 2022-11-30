d57b85fbc9ec79b407903bb17fb94dc54f1d7484
ValerioGoffry <67636564+ValerioGoffry@users.noreply.github.com>
2022-11-30 12:47:15 +0100

    Add files via upload

--- Sasso_Carta_Forbici.py
+++ Sasso_Carta_Forbici.py
@@ -2,38 +2,44 @@ import random
 
 mosse = ["sasso", "carta", "forbici"]
 scelta_pc = random.choice(mosse)
-scelta_utente = str(input("Scegli sasso,carta,forbici: "))
+
+print("Benvenuto al gioco sasso carta forbici")
+nome = input("Come ti chiami? ")
+print(f"Ottimo! {nome} si inizia a giocare!!")
 
 
-def pari():
-    if scelta_pc == "sasso" and scelta_utente == "sasso":
-        print("Parità")
-    elif scelta_pc == "carta" and scelta_utente == "carta":
-        print("Parità")
-    elif scelta_pc == "forbici" and scelta_utente == "forbici":
-        print("Parità")
-
-
-def pc():
-    if scelta_pc == "carta" and scelta_utente == "sasso":
-        print("Ha vinto il pc")
+def scelte(punteggio_giocatore=0, punteggio_pc=0):
+    if scelta_pc == scelta_utente:
+        print(f"Parità!")
+        punteggio_giocatore += 0
+        punteggio_pc += 0
+    elif scelta_pc == "carta" and scelta_utente == "sasso":
+        print("Ha vinto il computer")
+        punteggio_pc += 1
     elif scelta_pc == "forbici" and scelta_utente == "carta":
-        print("Ha vinto il pc")
+        print("Ha vinto il computer!")
+        punteggio_pc += 1
     elif scelta_pc == "sasso" and scelta_utente == "forbici":
-        print("Ha vinto il pc")
-
-
-def utente():
-    if scelta_utente == "carta" and scelta_pc == "sasso":
-        print("Ha vinto il Giocatore")
+        print("Ha vinto il computer!")
+        punteggio_pc += 1
+    elif scelta_utente == "carta" and scelta_pc == "sasso":
+        print(f"Ha vinto {nome}!")
+        punteggio_giocatore = punteggio_giocatore + 1
     elif scelta_utente == "forbici" and scelta_pc == "carta":
-        print("Ha vinto il Giocatore")
+        print(f"Ha vinto {nome}!")
+        punteggio_giocatore += 1
     elif scelta_utente == "sasso" and scelta_pc == "forbici":
-        print("Ha vinto il Giocatore")
+        print(f"Ha vinto {nome}!")
+        punteggio_giocatore += 1
+    print(f"Punteggio {nome}: {punteggio_giocatore}")
+    print(f"Punteggio PC: {punteggio_pc}")
 
 
-print(f"Giocatore: {scelta_utente}".lower())
-print(f"Pc: {scelta_pc}")
-pari()
-pc()
-utente()
+while True:
+    scelta_utente = input("Scegli sasso,carta,forbici: ")
+    if scelta_utente == "sasso" or scelta_utente == "carta" or scelta_utente:
+        print(f"{nome} ha scelto: {scelta_utente.lower()}")
+        print(f"Il Computer ha scelto: {scelta_pc}")
+        scelte()
+    else:
+        print("Devi per forza inserire sasso, carta o forbici")
