8fb562802322b8f76a22eeef40cbb0ef8b2fd9cc
ValerioGoffry <67636564+ValerioGoffry@users.noreply.github.com>
2022-11-30 13:29:10 +0100

    Add files via upload

--- Sasso_Carta_Forbici.py
+++ Sasso_Carta_Forbici.py
@@ -8,31 +8,21 @@ nome = input("Come ti chiami? ")
 print(f"Ottimo! {nome} si inizia a giocare!!")
 
 
-def scelte(punteggio_giocatore=0, punteggio_pc=0):
+def scelte():
     if scelta_pc == scelta_utente:
         print(f"Parità!")
-        punteggio_giocatore += 0
-        punteggio_pc += 0
     elif scelta_pc == "carta" and scelta_utente == "sasso":
         print("Ha vinto il computer")
-        punteggio_pc += 1
     elif scelta_pc == "forbici" and scelta_utente == "carta":
         print("Ha vinto il computer!")
-        punteggio_pc += 1
     elif scelta_pc == "sasso" and scelta_utente == "forbici":
         print("Ha vinto il computer!")
-        punteggio_pc += 1
     elif scelta_utente == "carta" and scelta_pc == "sasso":
         print(f"Ha vinto {nome}!")
-        punteggio_giocatore = punteggio_giocatore + 1
     elif scelta_utente == "forbici" and scelta_pc == "carta":
         print(f"Ha vinto {nome}!")
-        punteggio_giocatore += 1
     elif scelta_utente == "sasso" and scelta_pc == "forbici":
         print(f"Ha vinto {nome}!")
-        punteggio_giocatore += 1
-    print(f"Punteggio {nome}: {punteggio_giocatore}")
-    print(f"Punteggio PC: {punteggio_pc}")
 
 
 while True:
